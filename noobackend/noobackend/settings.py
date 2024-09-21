0645d698b05e43a6606f9ed9abc6cf5aec12a0ab
valmikGit <quantumcomputingiiitb@gmail.com>
2024-09-22 00:31:54 +0530

    debugging.

--- noobackend/noobackend/settings.py
+++ noobackend/noobackend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'nooapi',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
